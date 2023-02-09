61fd061c5a4b0ca7aa0cdedf19da9926dd54618e
merencio <felipe.smerencio@gmail.com>
2023-02-09 09:14:26 -0300

    backup

--- index.js
+++ index.js
@@ -3,48 +3,42 @@
 var AWS = require("aws-sdk");
 AWS.config.update({region: 'sa-east-1'});
 
-// AWS.config.getCredentials(function(err) {
-//     if (err) console.log(err.stack);
-//     // credentials not loaded
-//     else {
-//       console.log("Access key:", AWS.config.credentials.accessKeyId);
-//     }
-// });
-
-// console.log("Region: ", AWS.config.region);
-
 var transcribeservice = new AWS.TranscribeService();
 
 var params = {
-    CallAnalyticsJobName: 'labTranscribeAtivy', /* required */
+    CallAnalyticsJobName: 'labTranscribeAtivy312312312322', /* required */
     Media: { /* required */
-    MediaFileUri: 's3://test202302062/mono-conversasao_cliente_34999973687.mp3',
-    RedactedMediaFileUri: 's3://test202302062/mono-conversasao_cliente_34999973687.mp3'
+    MediaFileUri: 's3://test202302062-in2/mono-conversasao_cliente_34999973687.mp3',
+    RedactedMediaFileUri: 's3://test202302062-in2/mono-conversasao_cliente_34999973687.mp3'
 },
 ChannelDefinitions: [
     {
-        ChannelId: '5',
-        ParticipantRole: "CUSTOMER"
+        ChannelId: 0,
+        ParticipantRole: "AGENT",
+    },
+    {
+        ChannelId: 1,
+        ParticipantRole: "CUSTOMER",
     },
     /* more items */
 ],
-DataAccessRoleArn: 'arn:aws:s3:::test202302062',
+// DataAccessRoleArn: 'arn:aws:iam::566322147993:role/full-acess-trascribebatata',
 OutputEncryptionKMSKeyId: 'STRING_VALUE',
-OutputLocation: 'STRING_VALUE',
+OutputLocation: 's3://test202302062-out',
 Settings: {
     ContentRedaction: {
         RedactionOutput: "redacted", /* required */
         RedactionType: "PII", /* required */
         PiiEntityTypes: []
     },
-    LanguageIdSettings: {
-        '<LanguageCode>': {
-            LanguageModelName: 'STRING_VALUE',
-            VocabularyFilterName: 'STRING_VALUE',
-            VocabularyName: 'STRING_VALUE'
-        },
-        /* '<LanguageCode>': ... */
-    },
+    // LanguageIdSettings: {
+    //     '<LanguageCode>': {
+    //         LanguageModelName: 'STRING_VALUE',
+    //         VocabularyFilterName: 'STRING_VALUE',
+    //         VocabularyName: 'STRING_VALUE'
+    //     },
+    //     /* '<LanguageCode>': ... */
+    // },
     LanguageModelName: 'STRING_VALUE',
     LanguageOptions: [
         "pt-BR",
@@ -55,6 +49,8 @@ Settings: {
     VocabularyName: 'STRING_VALUE'
 }
 };
+
+console.log("Batata!");
 transcribeservice.startCallAnalyticsJob(params, function(err, data) {
 if (err) console.log(err, err.stack); // an error occurred
 else     console.log(data);           // successful response
